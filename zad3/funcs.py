1066abf3ed68521eeab9b0523e247db7af686f42
Evgeny Skorobogaty <sev.20@uni-dubna.ru>
2021-11-30 22:25:26 +0300

    [zad3] Migrate to matplotlib from pygraphviz

--- zad3/funcs.py
+++ zad3/funcs.py
@@ -1,25 +1,21 @@
+image_filename = "zad3_graph.png"
+
 def draw_graph(graph):
     import networkx as nx
-    from networkx.drawing.nx_agraph import to_agraph
+    import matplotlib.pyplot as plt
 
     G = nx.DiGraph() if graph.is_directed else nx.Graph()
     G.add_edges_from(graph.edges)
     G.add_nodes_from(graph.vertexes)
 
-    nx.draw_circular(G)
-
-    A = to_agraph(G)
-    A.layout("dot")
-    A.draw("zad3_graph.png")
+    nx.draw(G, with_labels=True)
+    plt.savefig(image_filename)
 
     from PIL import Image
-    im = Image.open("zad3_graph.png")
+    im = Image.open(image_filename)
     im.show()
 
-    # from os import system
-    # system("feh zad3_graph.png")
-
-
+from os import system
 def clear_screen():
-    from os import system
     system("cls||clear")
+
